9d516c3f322453b16d0f181f4b1fb53f289e402b
Abrar <abrarmurad00@gmail.com>
2024-03-07 23:47:21 -0500

    switched location to Toronto and added map layers

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, LayersControl } from "react-leaflet";
 import { defaultMarker } from "./defaultMarker";
 import "./Map.css";
 
-const center = [51.505, -0.09];
+const center = [43.653225, -79.383186];
+const union = [43.6452, -79.3806];
 
 const MapComp = ({ onOpen }) => {
   return (
     <MapContainer style={{ height: "100vh" }} center={center} zoom={13}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
+      <LayersControl position="topright">
+        {/* Base layers */}
+        <LayersControl.BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Additional Layer">
+          <TileLayer
+            url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
+        {/* Overlay layers (if any) */}
+      </LayersControl>
+      {/* Markers */}
       <Marker position={center} icon={defaultMarker}>
+        <Popup className="request-popup">
+          <button onClick={onOpen}>Open Modal</button>
+        </Popup>
+      </Marker>
+      <Marker position={union} icon={defaultMarker}>
         <Popup className="request-popup">
           <button onClick={onOpen}>Open Modal</button>
         </Popup>

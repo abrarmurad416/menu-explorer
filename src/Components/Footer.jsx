ae08ab123541ebe529bace487cc240120f45b329
Abrar <abrarmurad00@gmail.com>
2024-04-30 23:05:17 -0400

    fullscreen footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../index.css";
-import svgpattern from "../Components/svgpattern.svg";
 
 const Footer = () => {
     return (
         <div className='d-flex flex-column'>
-            <div classname="spacer">
-                <img src={svgpattern} alt="Your SVG" className="img-fluid" />
-            </div>
+            <svg id="visual" viewBox="0 0 900 160" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" className="layer spacer">
+            </svg>
             <div className="navbar-fixed-bottom">
                 <footer className="text-center text-lg-start">
                     <div className="text-center p-4 bg-dark text-white">
@@ -16,10 +14,11 @@ const Footer = () => {
                     </div>
                 </footer>
             </div>
-            <div className='p-4' style={{background: '#212529'}}>
+            <div className='p-4' style={{ background: '#212529' }}>
             </div>
         </div>
     );
 }
+
 
 export default Footer;
